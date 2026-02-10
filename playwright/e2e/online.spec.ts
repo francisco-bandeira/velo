667647bb19426badcad7e1e9547961670150797e
Francisco Bandeira <francisco.frazzon@outlook.com>
2026-02-10 17:58:24 -0300

    mudando o nome do cenario

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('wepapp deve estar online', async ({ page }) => {
+test('a wepapp deve estar online', async ({ page }) => {
   await page.goto('http://localhost:5173')
 
   await expect(page).toHaveTitle(/Velô by Papito/)
